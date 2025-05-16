6aaa8eb2b95cb52a774207bf723f7712a0ae9b7b
Gmpartners <seu-email@example.com>
2025-05-16 17:54:43 +0000

    Corrige duplicação da função getMetricsSummary

--- src/controllers/metricsController.js
+++ src/controllers/metricsController.js
@@ -1,75 +1,7 @@
-const { getMetricsSummary } = require("./metricsSummaryController");
-const mongoose = require("mongoose");
 const { Metrics, Account, Campaign, Event, Email } = require('../models');
 const responseUtils = require('../utils/responseUtil');
 const dateHelpers = require('../utils/dateHelpersUtil');
 
-// Obter resumo geral de métricas
-const getMetricsSummary = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    
-    if (!userId) {
-      return responseUtils.error(res, 'User ID é obrigatório');
-    }
-    
-    // Obter estatísticas gerais de emails
-    const emailCount = await Email.countDocuments({ userId });
-    const eventCount = await Event.countDocuments({ userId });
-    
-    // Contar eventos por tipo
-    const sentCount = await Event.countDocuments({ userId, eventType: 'send' });
-    const openCount = await Event.countDocuments({ userId, eventType: 'open' });
-    const clickCount = await Event.countDocuments({ userId, eventType: 'click' });
-    const bounceCount = await Event.countDocuments({ userId, eventType: 'bounce' });
-    const unsubscribeCount = await Event.countDocuments({ userId, eventType: 'unsubscribe' });
-    
-    // Contar eventos únicos (usando isFirstInteraction = true)
-    const uniqueOpenCount = await Event.countDocuments({ userId, eventType: 'open', isFirstInteraction: true });
-    const uniqueClickCount = await Event.countDocuments({ userId, eventType: 'click', isFirstInteraction: true });
-    
-    // Calcular taxas
-    const openRate = sentCount > 0 ? (openCount / sentCount) * 100 : 0;
-    const clickRate = sentCount > 0 ? (clickCount / sentCount) * 100 : 0;
-    const bounceRate = sentCount > 0 ? (bounceCount / sentCount) * 100 : 0;
-    const unsubscribeRate = sentCount > 0 ? (unsubscribeCount / sentCount) * 100 : 0;
-    
-    // Calcular taxas únicas
-    const uniqueOpenRate = sentCount > 0 ? (uniqueOpenCount / sentCount) * 100 : 0;
-    const uniqueClickRate = sentCount > 0 ? (uniqueClickCount / sentCount) * 100 : 0;
-    
-    // Calcular CTR (Click-Through Rate)
-    const ctr = openCount > 0 ? (clickCount / openCount) * 100 : 0;
-    const uniqueCtr = uniqueOpenCount > 0 ? (uniqueClickCount / uniqueOpenCount) * 100 : 0;
-    
-    return responseUtils.success(res, {
-      counts: {
-        emailCount,
-        eventCount,
-        sentCount,
-        openCount,
-        uniqueOpenCount,
-        clickCount,
-        uniqueClickCount,
-        bounceCount,
-        unsubscribeCount
-      },
-      rates: {
-        openRate,
-        uniqueOpenRate,
-        clickRate,
-        uniqueClickRate,
-        ctr,
-        uniqueCtr,
-        bounceRate,
-        unsubscribeRate
-      },
-      note: "Para métricas mais detalhadas, utilize os endpoints específicos: /by-date, /by-email, /by-account, etc."
-    });
-  } catch (err) {
-    return responseUtils.serverError(res, err);
-  }
-};
 
 
 // Obter métricas por data
@@ -1365,7 +1297,6 @@ const getEvents = async (req, res) => {
 
 module.exports = {
   compareMetrics,
-  getMetricsSummary,
   getMetricsByDate,
   getMetricsByAccount,
   getMetricsByCampaign,
